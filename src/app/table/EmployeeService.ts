6e2d0f02ec0caa6e0515da6621d61654c8dde5ea
yongsu <yongsumaid@outlook.com>
2018-08-08 18:40:04 +0530

    cc

--- src/app/table/EmployeeService.ts
+++ src/app/table/EmployeeService.ts
@@ -14,7 +14,7 @@ export class EmployeeService {
 
     constructor(private _http: HttpClient) {
         this.myheaders = new Headers();
-        this.myheaders.append('Access-Control-Allow-Origin', 'http://localhost:4200');
+        this.myheaders.append('Access-Control-Allow-Origin', 'http://35.154.157.254:80');
         let options = new RequestOptions({ headers: this.myheaders })
 
     }
